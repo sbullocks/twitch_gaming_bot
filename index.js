61e4688902f2496fa8a2af9cde7f8025b92ede3c
sbullocks <sbullocks@gmail.com>
2023-01-13 00:04:56 -0500

    updated tags to userstate

--- index.js
+++ index.js
@@ -1,104 +1,4 @@
-// require('dotenv').config();
-// const tmi = require('tmi.js');
-
-// const opts = {
-//     identity: {
-//         // username: 'your_bot_username',
-//         // password: 'oauth:YOUR_OAUTH_TOKEN'
-//         username: 'paid4hire',
-//         password: 'oauth:uob9ay4yr2l4ib491wxnq52b37r09f'
-//     },
-//     channels: [
-//         // 'channel_name_to_join'
-//         'paid4hire'
-
-//     ]
-// };
-
-// const client = new tmi.Client(opts);
-
-// client.on('error', (err) => {
-//     console.error(`Error: ${err}`);
-// });
-
-// try {
-//     client.connect()
-//         .then(() => {
-//             console.log('connected!');
-//         }).catch(err => {
-//             console.error(`Error: ${err}`);
-//         });
-// } catch (err) {
-//     console.error(`Error: ${err}`);
-// }
-
-// require('dotenv').config();
-// const tmi = require('tmi.js');
-
-// const client = new tmi.Client({
-// 	options: { debug: true },
-// 	identity: {
-// 		USERNAME: process.env.USERNAME + Math.floor(Math.random()*100000),
-// 		PASSWORD: process.env.OAUTH_TOKEN
-// 	},
-//     channels: [
-//                 // 'channel_name_to_join'
-//                 'paid4hire'
-        
-//             ]
-// });
-
-// client.connect();
-
-// client.on('connected', (addr, port) => {
-//     console.log(`Connected to ${addr}:${port}`);
-// });
-
-// client.on('message', (channel, tags, message, self) => {
-//     // Ignore echoed messages.
-//     if(self) return;
-
-//     if(message.toLowerCase() === '!hello') {
-//         // "@alca, heya!"
-//         client.say(channel, `@${tags.username}, heya!`);
-//     }
-// });
-
-// client.on('disconnected', (reason) => {
-//     console.log('Disconnected due to: '+ reason);
-// });
-
-// client.connect();
-
-// // Create a client with our options
-// const client = new tmi.client(opts);
-
-// // Register our event handlers (defined below)
-// client.on('message', onMessageHandler);
-// client.on('connected', onConnectedHandler);
-
-// // Connect to Twitch:
-// client.connect();
-
-// // Called every time a message comes in
-// function onMessageHandler (target, context, msg, self) {
-//   if (self) { return; } // Ignore messages from the bot
-
-//   // Remove whitespace from chat message
-//   const commandName = msg.trim();
-
-//   // If the command is known, let's execute it
-//   if (commandName === '!hello') {
-//     client.say(target, `Hello, ${context.username}!`);
-//     console.log(`* Executed ${commandName} command`);
-//   } else {
-//     console.log(`* Unknown command ${commandName}`);
-//   }
-// }
-
 require('dotenv').config();
-// const {isBadWord} = require('./badWords');
-// const badWords = require('./badWords');
 const { isBadWord } = require('./badWords');
 const tmi = require('tmi.js');
 
@@ -121,17 +21,7 @@ client.on('connected', (addr, port) => {
     console.log(`Connected to ${addr}:${port}`);
 });
 
-// client.on('message', (channel, tags, message, self) => {
-//     // Ignore echoed messages
-//     if(self) return;
-
-//     // Check for the !hello command
-//     if(message.toLowerCase() === '!hello') {
-//         client.say(channel, `@${tags.username}, heya!`);
-//     }
-// });
-
-client.on('message', (channel, tags, message, self) => {
+client.on('message', (channel, userstate, message, self) => {
     // Ignore messages from the bot
     if (self) return;
 
@@ -139,12 +29,15 @@ client.on('message', (channel, tags, message, self) => {
     if (isBadWord(message)) {
         // take appropriate action
         // block the message or timeout the user 
-        console.log("bad word found")
+        if(userstate.username) {
+            client.say(channel, `@${userstate.username}, please do not use bad language`)
+                .catch(error => console.log(error));
+        }
     }
 
     // Check for the !hello command
     if(message.toLowerCase() === '!hello') {
-        client.say(channel, `@${tags.username}, heya!`).catch(error=> console.log(error));
+        client.say(channel, `@${userstate.username}, heya!`).catch(error=> console.log(error));
     }
 });
 
@@ -154,6 +47,9 @@ client.on('disconnected', (reason) => {
 
 // Connect to Twitch
 client.connect()
+.then(() => {
+    console.log("Connected Successfully!");
+})
 .catch(err => {
     console.error(`Error: ${err}`);
 });
